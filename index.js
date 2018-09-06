4a2e7e4b3356f95d3bafb147b197f9ba0fdb5c45
Fernanda Caroline <fe.caroline90@gmail.com>
2018-09-06 00:47:03 -0300

    bilbioteca números romanos

---
+++ index.js
@@ -0,0 +1,28 @@
+exports.intToRoman = function intToRoman(num) {
+  var result = '';
+  var decimal = [5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["V̅","I̅V̅","M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  for (var i = 0;i<=decimal.length;i++) {
+    while (num%decimal[i] < num) {
+      result += roman[i];
+      num -= decimal[i];
+    }
+  }
+  return result;
+}
+
+exports.romanToInt = function romanToInt(rom){
+
+  var result = 0;
+  var decimal = [5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["V̅","I̅V̅","M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  // rom.toUpperCase();
+  for (var i = 0;i<=decimal.length;i++) {
+    while (rom.indexOf(roman[i]) === 0){
+      result += decimal[i];
+      rom = rom.replace(roman[i],'');
+
+    }
+  }
+  return result;
+}
